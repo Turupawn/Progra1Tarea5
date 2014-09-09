443bb751fbaafa24603faad4e24f272c87438420
Turupawn <turupawn@userr>
2014-09-08 23:35:59 -0600

    primer commit

---
+++ src/Ejercicios.java
@@ -0,0 +1,51 @@
+
+public class Ejercicios
+{
+	//Devuelve el valor de mi_arreglo en la posicion [columna][fila]
+	static String obtenerValor(String mi_arreglo[][], int columna, int fila)
+	{
+		return "";
+	}
+	
+	//Devuelve la cantidad de columnas de mi_arreglo
+	static int obtenerCantidadColumnas(double mi_arreglo[][])
+	{
+		return -1;
+	}
+	
+	//Devuelve la cantidad de filas de mi_arreglo
+	static int obtenerCantidadFilas(double mi_arreglo[][])
+	{
+		return -1;
+	}
+	
+	//Devuelve la cantidad de elementos que puede alacenar mi_arreglo
+	static int obtenerCantidadElementos(double mi_arreglo[][])
+	{
+		return -1;
+	}
+	
+	//Devuelve la sua de todos los elementos de mi_arreglo
+	static double sumaElementos(double mi_arreglo[][])
+	{
+		return -1;
+	}
+	
+	//Devuelve el promedio de todos los elementos de mi_arreglo
+	static double promedioElementos(double mi_arreglo[][])
+	{
+		return -1;
+	}
+	
+	//Devuelve verdadero si num esta en mi_arreglo, de lo contrario devuelve false
+	static boolean existe(double mi_arreglo[][], int num)
+	{
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
